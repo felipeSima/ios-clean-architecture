241c9e212392a9df8646d79f6e6f9833d56277e6
Felipe Silva Lima <felipe.alexandre.sima@gmail.com>
2020-12-01 16:06:00 -0300

    Added the assembler to the project

--- ios-base-architecture/core/di/DepedencyInjection.swift
+++ ios-base-architecture/core/di/DepedencyInjection.swift
@@ -9,11 +9,6 @@ import Foundation
 import Swinject
 
 class DepedencyInjection {
-
-    /**
-     The container constant is the responsible parameter to handle the Dependency injection in the project.
-     */
-
     static var container: Container = {
         let container = Container()
 
@@ -31,5 +26,33 @@ class DepedencyInjection {
         container.register(GetPokemonDetail.self) { r in GetPokemonDetailImpl(repository: r.resolve(PokemonDetailRepository.self)!)}
         return container
     }()
+}
+
+extension Assembler {
+    static let sharedAssembler: Assembler = {
+        let container = Container()
+        let assembler = Assembler([
+            PokemonListAssembler(),
+            PokemonDetailAssembler()
+        ], container: container)
+        return assembler
+    }()
+}
+
+class PokemonListAssembler: Assembly {
+    func assemble(container: Container) {
+        container.register(PokemonListRemoteDatasource.self) { _ in PokemonListRemoteDatasourceImpl()}
+        container.register(PokemonListLocalDataSource.self) { _ in PokemonListLocalDataSourceImpl()}
+        container.register(PokemonListRepository.self) { r in PokemonListRepositoryImpl(remoteDatasource: r.resolve(PokemonListRemoteDatasource.self)!, localDatasource: r.resolve(PokemonListLocalDataSource.self)!)}
+        container.register(GetPokemonList.self) { r in GetPokemonListImpl(repository: r.resolve(PokemonListRepository.self)!)}
+    }
+}
+
+class PokemonDetailAssembler: Assembly {
+    func assemble(container: Container) {
+        container.register(PokemonDetailRemoteDatasource.self) { _ in PokemonDetailRemoteDatasourceImpl()}
+        container.register(PokemonDetailRepository.self) { r in PokemonDetailRepositoryImpl(datasource: r.resolve(PokemonDetailRemoteDatasource.self)!)}
+        container.register(GetPokemonDetail.self) { r in GetPokemonDetailImpl(repository: r.resolve(PokemonDetailRepository.self)!)}
+    }
 }
 
